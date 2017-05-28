3744d4c51eb526f2a9ab5e2a634b7f9ffebb606b
nsokolovskiy <sokolovskynik@gmail.com>
2017-05-28 16:10:07 +0300

    fix problem with server

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var express = require("express");
 var app = express();
 var path = require('path');
-var setting = require('./setting.js'); //you have to create file which exports json object with settings
-
+var config = require('./setting.js'); //you have to create file which exports json object with settings
 var sql = require('mssql');
-var config = {
-  user: setting.user,
-  password: setting.password,
-  server: setting.server,
-  db: setting.db,
-};
 
 app.set('view engine', 'ejs');
 
@@ -17,15 +10,14 @@ app.set('view engine', 'ejs');
 //https://github.com/patriksimek/node-mssql#connection-pools
 
 app.get('/', function(req,res) {
-  const pool1 = new sql.ConnectionPool(config, err=> {
+  const pool1 = new sql.ConnectionPool(config, err => {
     var request = new sql.Request(pool1);
-    request.query('select * from backlog', function (err, recordset){
+    request.query('select * from backlog order by priority DESC', function (err, recordset) {
       res.render(path.join(__dirname+'/view/index.ejs'), {backlog: recordset});
     });
   });
 });
 
-
 app.listen(3000, function(){ //
-    console.log('server is started');
+    console.log('Server is started');
 });
